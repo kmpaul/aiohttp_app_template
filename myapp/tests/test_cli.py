be2ab0b3832106736ec3b68c0e5f4a60b9c791aa
Kevin Paul <kpaul@ucar.edu>
2020-03-31 08:57:36 -0600

    Change env var prefix

--- myapp/tests/test_cli.py
+++ myapp/tests/test_cli.py
@@ -2,21 +2,23 @@ from myapp.cli import DEFAULT_CONFIG, cli
 
 
 def test_cli_defaults():
-    ctx = cli.make_context(cli.name, args=[], auto_envvar_prefix='APP')
+    ctx = cli.make_context(cli.name, args=[], auto_envvar_prefix='MYAPP')
     assert ctx.params == DEFAULT_CONFIG
 
 
 def test_cli_host():
     params = DEFAULT_CONFIG.copy()
     params['host'] = 'http://localhost'
-    ctx = cli.make_context(cli.name, args=['--host', params['host']], auto_envvar_prefix='APP')
+    ctx = cli.make_context(cli.name, args=['--host', params['host']], auto_envvar_prefix='MYAPP')
     assert ctx.params == params
 
 
 def test_cli_port():
     params = DEFAULT_CONFIG.copy()
     params['port'] = 4567
-    ctx = cli.make_context(cli.name, args=['--port', str(params['port'])], auto_envvar_prefix='APP')
+    ctx = cli.make_context(
+        cli.name, args=['--port', str(params['port'])], auto_envvar_prefix='MYAPP'
+    )
     assert ctx.params == params
 
 
@@ -24,7 +26,7 @@ def test_cli_logging():
     params = DEFAULT_CONFIG.copy()
     params['logging'] = 'WARNING'
     ctx = cli.make_context(
-        cli.name, args=['--logging', str(params['logging'])], auto_envvar_prefix='APP'
+        cli.name, args=['--logging', str(params['logging'])], auto_envvar_prefix='MYAPP'
     )
     assert ctx.params == params
 
@@ -33,7 +35,7 @@ def test_cli_mongouri():
     params = DEFAULT_CONFIG.copy()
     params['mongouri'] = 'mongodb://test.mongodb.com'
     ctx = cli.make_context(
-        cli.name, args=['--mongouri', params['mongouri']], auto_envvar_prefix='APP'
+        cli.name, args=['--mongouri', params['mongouri']], auto_envvar_prefix='MYAPP'
     )
     assert ctx.params == params
 
@@ -42,7 +44,7 @@ def test_cli_mongodb():
     params = DEFAULT_CONFIG.copy()
     params['mongodb'] = 'test'
     ctx = cli.make_context(
-        cli.name, args=['--mongodb', params['mongodb']], auto_envvar_prefix='APP'
+        cli.name, args=['--mongodb', params['mongodb']], auto_envvar_prefix='MYAPP'
     )
     assert ctx.params == params
 
@@ -51,7 +53,7 @@ def test_cli_env(cli_env):
     params = DEFAULT_CONFIG.copy()
     params['host'] = 'http://127.0.0.1'
     params['port'] = 6789
-    ctx = cli.make_context(cli.name, args=[], auto_envvar_prefix='APP')
+    ctx = cli.make_context(cli.name, args=[], auto_envvar_prefix='MYAPP')
     assert ctx.params == params
 
 
@@ -59,7 +61,9 @@ def test_cli_env_overridden(cli_env):
     params = DEFAULT_CONFIG.copy()
     params['host'] = 'http://127.0.0.1'
     params['port'] = 9999
-    ctx = cli.make_context(cli.name, args=['--port', str(params['port'])], auto_envvar_prefix='APP')
+    ctx = cli.make_context(
+        cli.name, args=['--port', str(params['port'])], auto_envvar_prefix='MYAPP'
+    )
     assert ctx.params == params
 
 
@@ -67,7 +71,7 @@ def test_cli_configfile(cli_configfile):
     params = DEFAULT_CONFIG.copy()
     params['host'] = 'http://0.0.0.0'
     params['port'] = 9999
-    ctx = cli.make_context(cli.name, args=['--config', cli_configfile], auto_envvar_prefix='APP')
+    ctx = cli.make_context(cli.name, args=['--config', cli_configfile], auto_envvar_prefix='MYAPP')
     assert ctx.params == params
 
 
@@ -78,7 +82,7 @@ def test_cli_configfile_overridden(cli_configfile):
     ctx = cli.make_context(
         cli.name,
         args=['--config', cli_configfile, '--port', str(params['port'])],
-        auto_envvar_prefix='APP',
+        auto_envvar_prefix='MYAPP',
     )
     assert ctx.params == params
 
@@ -90,6 +94,6 @@ def test_cli_env_configfile_overridden(cli_env, cli_configfile):
     ctx = cli.make_context(
         cli.name,
         args=['--config', cli_configfile, '--port', str(params['port'])],
-        auto_envvar_prefix='APP',
+        auto_envvar_prefix='MYAPP',
     )
     assert ctx.params == params
